cafd27215e66ef46f60fd344276c58b57673db60
Obay <obay.kadour999@gmail.com>
2024-03-21 16:38:31 +0200

    adding the second project image

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ window.onscroll = () => {
     if (top >= offset && top < offset + height) {
       navLinks.forEach(links => {
         links.classList.remove('active');
-        // Make sure to include "#" in the query selector to select id
         document.querySelector('header nav ul li a[href*="#' + id + '"]').classList.add('active');
       });
     }
@@ -28,14 +27,11 @@ menuIcon.onclick = () => {
 
 
 let navbarLinks = document.querySelectorAll(".navbar a");
-// Assuming 'navLinks' is your NodeList of anchor tags as defined previously
 navbarLinks.forEach(link => {
   link.addEventListener('click', function () {
-    // Remove 'active' class from all links
     navbarLinks.forEach(link => {
       link.classList.remove('active');
     });
-    // Add 'active' class to the clicked link
     this.classList.add('active');
   });
 });
@@ -45,8 +41,6 @@ navbarLinks.forEach(link => {
 document.addEventListener('scroll', function () {
   var header = document.querySelector('header');
   var scrolled = window.scrollY > 0;
-
-  // Toggle the 'scrolled' class based on scroll position
   header.classList.toggle('scrolled', scrolled);
 });
 
